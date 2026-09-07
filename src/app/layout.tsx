0e64068c522b9b1e3658b5f1c63f1477f9ac7372
kiran <kirkmar078@gmail.com>
2026-09-07 11:40:00 +0530

    fix(rb2b): add data-cfasync false to bypass Cloudflare Rocket Loader for pristine raw HTML script execution

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -92,8 +92,9 @@ export default function RootLayout({
           }}
         />
         <script
+          data-cfasync="false"
           dangerouslySetInnerHTML={{
-            __html: `!function(key){if(window.reb2b)return;window.reb2b={loaded:!0};var s=document.createElement("script");s.async=!0;s.src="https://ddwl4m2hdecbv.cloudfront.net/b/"+key+"/"+key+".js.gz";document.getElementsByTagName("script")[0].parentNode.insertBefore(s,document.getElementsByTagName("script")[0])}("DNXY8HJJYVO0");`,
+            __html: `!function(key){if(window.reb2b)return;window.reb2b={loaded:!0};var s=document.createElement("script");s.async=!0;s.setAttribute("data-cfasync","false");s.src="https://ddwl4m2hdecbv.cloudfront.net/b/"+key+"/"+key+".js.gz";document.getElementsByTagName("script")[0].parentNode.insertBefore(s,document.getElementsByTagName("script")[0])}("DNXY8HJJYVO0");`,
           }}
         />
       </head>
